d3815f7e8a364e30e68ed992b929ba687503b514
maiconpavi <75434446+maiconpavi@users.noreply.github.com>
2023-04-08 20:08:37 -0300

    customizing rust-tools

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -3,24 +3,21 @@ return {
   -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
-    -- require()
     opts = {
-      ensure_installed = { "lua_ls", "rust_analyzer" },
+      ensure_installed = { "lua_ls" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
     opts = {
       ensure_installed = { "prettier", "stylua", "rustfmt", "black" },
     },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
-      ensure_installed = { "python", "codelldb" },
+      ensure_installed = { "python" },
     },
   },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -15,39 +15,54 @@ return {
   {
     "simrat39/rust-tools.nvim",
     ft = { "rust" },
+    init = function() utils.list_insert_unique(astronvim.lsp.skip_setup, "rust_analyzer") end,
     config = function()
-      require("rust-tools").setup {
+      local rt = require "rust-tools"
+      local package_path = require("mason-registry").get_package("codelldb"):get_install_path()
+      local codelldb_path = package_path .. "/codelldb"
+      local liblldb_path = package_path .. "/extension/lldb/lib/liblldb"
+      local server = require("astronvim.utils.lsp").config "rust_analyzer"
+
+      server = vim.tbl_deep_extend("force", server, {
+        on_attach = function(client, bufnr) require("astronvim.utils.lsp").on_attach(client, bufnr) end,
+        settings = {
+          ["rust-analyzer"] = {
+            check = {
+              command = "clippy",
+            },
+            procMacro = {
+              enable = true,
+            },
+            cargo = {
+              loadOutDirsFromCheck = true,
+            },
+            assist = {
+              importGranularity = "module",
+              importPrefix = "by_self",
+            },
+          },
+        },
+      })
+
+      rt.setup {
         tools = {
           autoSetHints = true,
           runnables = {
             use_telescope = true,
           },
           inlay_hints = {
-            show_parameter_hints = true,
+            max_len_align = true,
+            show_parameter_hints = false,
             parameter_hints_prefix = "<- ",
             other_hints_prefix = "=> ",
           },
-          hover_actions = {
-            border = {
-              { "╭", "FloatBorder" },
-              { "─", "FloatBorder" },
-              { "╮", "FloatBorder" },
-              { "│", "FloatBorder" },
-              { "╯", "FloatBorder" },
-              { "─", "FloatBorder" },
-              { "╰", "FloatBorder" },
-              { "│", "FloatBorder" },
-            },
-          },
         },
-        server = {
-          settings = {
-            ["rust-analyzer"] = {
-              check = {
-                command = "clippy",
-              },
-            },
-          },
+        server = server,
+        dap = {
+          adapter = require("rust-tools.dap").get_codelldb_adapter(
+            codelldb_path,
+            liblldb_path .. (vim.loop.os_uname().sysname == "Linux" and ".so" or ".dylib")
+          ),
         },
       }
     end,
