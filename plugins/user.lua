cfb8e92740252810a7e65a8873d8161700e85d8c
maiconpavi <75434446+maiconpavi@users.noreply.github.com>
2023-10-01 07:26:19 -0300

    customizing status line

--- plugins/user.lua
+++ plugins/user.lua
@@ -8,20 +8,18 @@ return {
       opts.statusline = {                                                            -- statusline
         hl = { fg = "fg", bg = "bg" },
         status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
-        status.component.git_branch(),
-        status.component.file_info { filetype = {}, filename = false, file_modified = false },
         status.component.git_diff(),
         status.component.diagnostics(),
         status.component.fill(),
         status.component.cmd_info(),
         status.component.fill(),
         status.component.lsp { lsp_progress = false, surround = { separator = "right" } },
-        status.component.treesitter(),
-        status.component.nav(),
-        -- remove the 2nd mode indicator on the right
+        status.component.nav {
+          percentage = false,
+          scrollbar = false,
+        },
       }
 
-      -- return the final configuration table
       return opts
     end,
   },
