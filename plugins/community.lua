5379359c4ec8ac0778163d0f84e4a0f8ae0e399e
Maicon <maicon@pop-os.localdomain>
2023-10-05 11:46:11 -0300

    updating flash-nvim

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,7 +7,6 @@ return {
   { import = "astrocommunity.pack.full-dadbod" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.editing-support.vim-move" },
-  { import = "astrocommunity.motion.flash-nvim" },
 
   -- color
   -- { import = "astrocommunity.color.twilight-nvim" },
--- plugins/user.lua
+++ plugins/user.lua
@@ -2,6 +2,38 @@ local utils = require "user.utils"
 
 return {
   {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    vscode = true,
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function() require("flash").jump() end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function() require("flash").treesitter() end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function() require("flash").remote() end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function() require("flash").treesitter_search() end,
+        desc = "Treesitter Search",
+      },
+    },
+  },
+  {
     "rebelot/heirline.nvim",
     opts = function(_, opts)
       local status = require "astronvim.utils.status"
