c8e16f4b550b2c9385fb976bf58968d9ce9ee60e
Maicon <maicon@pop-os.localdomain>
2023-10-09 07:27:35 -0300

    reverting telescope config to default from astronvim

--- plugins/user.lua
+++ plugins/user.lua
@@ -194,10 +194,39 @@ return {
       "debugloop/telescope-undo.nvim",
     },
     config = function()
+      local actions = require "telescope.actions"
+      local get_icon = require("astronvim.utils").get_icon
       require("telescope").setup {
         defaults = {
           file_ignore_patterns = {
             "node_modules",
+          },
+          git_worktrees = vim.g.git_worktrees,
+          prompt_prefix = string.format("%s ", get_icon "Search"),
+          selection_caret = string.format("%s ", get_icon "Selected"),
+          path_display = { "truncate" },
+          sorting_strategy = "ascending",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              preview_width = 0.55,
+            },
+            vertical = {
+              mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+          },
+
+          mappings = {
+            i = {
+              ["<C-n>"] = actions.cycle_history_next,
+              ["<C-p>"] = actions.cycle_history_prev,
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+            },
+            n = { ["q"] = actions.close },
           },
         },
         extensions = {
