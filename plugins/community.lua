95b7f9a2d63013f7536c9740092669fd1db53cb8
maiconpavi <75434446+maiconpavi@users.noreply.github.com>
2023-11-26 20:49:49 -0300

    removing dead code

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,36 +2,10 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.rust" },
-  -- { import = "astrocommunity.pack.java" },
-  -- {
-  --   "mfussenegger/nvim-jdtls",
-  --   opts = {
-  --     settings = {
-  --       java = {
-  --         configuration = {
-  --           runtimes = {
-  --             {
-  --               name = "JavaSE-19",
-  --               path = "/usr/lib/jvm/java-19-openjdk-amd64/",
-  --             },
-  --             {
-  --               name = "JavaSE-11",
-  --               path = "/usr/lib/jvm/java-11-openjdk-amd64/",
-  --             },
-  --             {
-  --               name = "JDK-21",
-  --               path = "/usr/lib/jvm/jdk-21-oracle-x64/",
-  --             },
-  --           },
-  --         },
-  --       },
-  --     },
-  --   },
-  -- },
-  -- { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.pack.full-dadbod" },
   { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.haskell" },
 
   -- color
   -- { import = "astrocommunity.color.twilight-nvim" },
@@ -39,8 +13,8 @@ return {
 
   -- colorschemes
   { import = "astrocommunity.colorscheme.kanagawa-nvim" },
-  { import = "astrocommunity.colorscheme.vscode-nvim" },
-  { import = "astrocommunity.colorscheme.github-nvim-theme" },
-  { import = "astrocommunity.colorscheme.oxocarbon-nvim" },
-  { import = "astrocommunity.colorscheme.dracula-nvim" },
+  -- { import = "astrocommunity.colorscheme.vscode-nvim" },
+  -- { import = "astrocommunity.colorscheme.github-nvim-theme" },
+  -- { import = "astrocommunity.colorscheme.oxocarbon-nvim" },
+  -- { import = "astrocommunity.colorscheme.dracula-nvim" },
 }
