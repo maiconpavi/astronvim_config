8580aec2876c40014ccec5306dcf9d33ab6af8a3
maicon <maicon@pop-os.localdomain>
2024-03-22 09:51:44 -0300

    removing copilot from cmp

--- init.lua
+++ init.lua
@@ -140,6 +140,19 @@ return {
           vim.opt_local.tabstop = 2
         end,
       })
+      require("actions-preview").setup {
+        telescope = {
+          sorting_strategy = "ascending",
+          layout_strategy = "vertical",
+          layout_config = {
+            width = 0.8,
+            height = 0.9,
+            prompt_position = "top",
+            preview_cutoff = 20,
+            preview_height = function(_, _, max_lines) return max_lines - 15 end,
+          },
+        },
+      }
       --   extension = {
       --     foo = "fooscript",
       --
--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -6,7 +6,7 @@ return {
     cmd = "Copilot",
     event = "User AstroFile",
     opts = {
-      suggestion = { auto_trigger = true, debounce = 150 },
+      suggestion = { auto_trigger = true },
       filetypes = {
         yaml = true,
         markdown = true,
@@ -22,74 +22,6 @@ return {
         ["."] = false,
       },
     },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "zbirenbaum/copilot.lua" },
-    opts = function(_, opts)
-      local cmp, copilot = require "cmp", require "copilot.suggestion"
-      local snip_status_ok, luasnip = pcall(require, "luasnip")
-      if not snip_status_ok then return end
-      local function has_words_before()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-      end
-      cmp.setup {
-        enabled = function()
-          return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
-        end,
-      }
-
-      cmp.setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
-        sources = {
-          { name = "dap" },
-        },
-      })
-      if not opts.mapping then opts.mapping = {} end
-      opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
-        if copilot.is_visible() then
-          copilot.accept()
-        elseif cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end, { "i", "s" })
-
-      opts.mapping["<C-x>"] = cmp.mapping(function()
-        if copilot.is_visible() then copilot.next() end
-      end)
-
-      opts.mapping["<C-z>"] = cmp.mapping(function()
-        if copilot.is_visible() then copilot.prev() end
-      end)
-
-      opts.mapping["<C-right>"] = cmp.mapping(function()
-        if copilot.is_visible() then copilot.accept_word() end
-      end)
-
-      opts.mapping["<C-l>"] = cmp.mapping(function()
-        if copilot.is_visible() then copilot.accept_word() end
-      end)
-
-      opts.mapping["<C-down>"] = cmp.mapping(function()
-        if copilot.is_visible() then copilot.accept_line() end
-      end)
-
-      opts.mapping["<C-j>"] = cmp.mapping(function()
-        if copilot.is_visible() then copilot.accept_line() end
-      end)
-
-      opts.mapping["<C-c>"] = cmp.mapping(function()
-        if copilot.is_visible() then copilot.dismiss() end
-      end)
-
-      return opts
-    end,
   },
   {
     "aznhe21/actions-preview.nvim",
